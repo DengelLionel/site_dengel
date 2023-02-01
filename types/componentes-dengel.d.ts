f955899d0e1363221786e71b0b1762f627b8b04e
Dengel <52581244+DengelLionel@users.noreply.github.com>
2023-01-31 23:39:12 -0500

    aver

--- types/componentes-dengel.d.ts
+++ types/componentes-dengel.d.ts
@@ -1,6 +1,6 @@
 declare module 'componentes-dengel' {
     // Aquí puedes declarar las propiedades y métodos que están disponibles en el módulo 'componentes-dengel'
     // Por ejemplo, si el módulo tiene una propiedad 'version' y un método 'printVersion', podrías escribir:
-    const HeaderMobileTypeOne,NavMobileTypeOne: string;
+    const HeaderMobileTypeOne,NavMobileTypeOne: any;
     function printVersion(): void;
   }
