64fc9b047c3ed957c84fcc860f8988a56d876807
Dengel <52581244+DengelLionel@users.noreply.github.com>
2023-01-31 23:36:41 -0500

    reparé creo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:["fotos.perfil.com","kardumtech.es","www.cybercenter.cl"]
+    
+   }
 }
 
 module.exports = nextConfig
---
+++ types/componentes-dengel.d.ts
@@ -0,0 +1,6 @@
+declare module 'componentes-dengel' {
+    // Aquí puedes declarar las propiedades y métodos que están disponibles en el módulo 'componentes-dengel'
+    // Por ejemplo, si el módulo tiene una propiedad 'version' y un método 'printVersion', podrías escribir:
+    const HeaderMobileTypeOne,NavMobileTypeOne: string;
+    function printVersion(): void;
+  }
