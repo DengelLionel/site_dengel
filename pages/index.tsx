ba2e0d9e4c6eaa17d37ce95389b4b15c9d0293e6
Dengel <52581244+DengelLionel@users.noreply.github.com>
2023-02-07 12:09:46 -0500

    advance

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import {HeaderMobileTypeOne,NavMobileTypeOne} from "componentes-dengel" 
+import {HeaderTwo,NavDesktopTypeTwo,NavMobileTypeTwo} from "componentes-dengel" 
 import { useState } from 'react'
 import "componentes-dengel/dist/style.css" 
 
 export default function Home() {
 
 const[openMenu,setOpenMenu]=useState(false)
-
+const[openMenuDesktop,setOpenMenuDesktop]=useState(false)
   return (
 
     <div>
@@ -20,18 +20,20 @@ const[openMenu,setOpenMenu]=useState(false)
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HeaderMobileTypeOne
-      setOpenMenu={()=>setOpenMenu(!openMenu)}
-      openMenu={openMenu}
-      colorHeader="bg-blue-700"
-      logo={<Link href='/'><Image width={30} height={30} alt='image'  src={'next.svg'}/></Link>}
-      > 
-        
-     <NavMobileTypeOne openMenu={openMenu}>
+      <HeaderTwo logo={<Link href='/'><Image width={30} height={30} alt='image'  src={'next.svg'}/></Link>} LinksNavDesktop={<section><h2>mundial</h2></section>} colorHeader='bg-blue-800' openMenuDesktop={openMenuDesktop}  openMenu={openMenu} navDesktopLittle={<NavDesktopTypeTwo openMenuDesktop={openMenuDesktop}>
+      <Link className='text-white' href={"#"}>Contactos</Link>
           <Link className='text-white' href={"#"}>Contactos</Link>
-          <Link className='text-white' href={"#"}>Contactos</Link>
-        </NavMobileTypeOne>
-      </HeaderMobileTypeOne> 
+  </NavDesktopTypeTwo>} setOpenMenu={()=>{setOpenMenu(!openMenu)}} setOpenMenuDesktop={()=>{setOpenMenuDesktop(!openMenuDesktop)}}>
+    
+    <NavMobileTypeTwo openMenu={openMenu}>
+      <h2 className='text-white'>contactos</h2>
+      <h2 className='text-white'>profile</h2>
+      <h2 className='text-white'>contactos</h2>
+      <h2 className='text-white'>profile</h2>
+    </NavMobileTypeTwo>
+
+    
+    </HeaderTwo>
     
       <main className=''>
      <h2>hola</h2>
--- types/componentes-dengel.d.ts
+++ types/componentes-dengel.d.ts
@@ -1,5 +1,8 @@
 declare module 'componentes-dengel' {
     export const HeaderMobileTypeOne: any;
     export const NavMobileTypeOne: any;
- 
+    export const HeaderTwo: any;
+    export const  NavDesktopTypeTwo: any;
+    export const  NavMobileTypeTwo: any;
+   
   }
