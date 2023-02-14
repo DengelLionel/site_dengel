67d350011d2e56d83214e6360503b0082f42e7e2
Dengel <52581244+DengelLionel@users.noreply.github.com>
2023-02-13 20:49:21 -0500

    ok

--- components/FooterContent.tsx
+++ components/FooterContent.tsx
@@ -5,16 +5,19 @@ import IconWhatsapp from "./Icons/IconWhatsapp"
 import IconTelegram from "./Icons/IconTelegram"
 import IconInstagram from "./Icons/IconInstagram"
 import IconTiktok from "./Icons/IconTiktok"
+import Link from "next/link"
 const FooterContent = () => {
   return (
    <section className="flex flex-col justify-center items-center">
     <h3 className={styles.titulo}>Contactame</h3>
             <ContenedorDatosFooter>
-            <IconEmail/>
-            <IconTelegram/>       
-            <IconWhatsapp/>
-            <IconInstagram/>   
-            <IconTiktok/>   
+            <Link href="mailto:denriveracajincho@gmail.com?Subject=Quiero%20contactarme%20contigo%20Dengel" target="_blank"><IconEmail/></Link>
+            <Link href="https://t.me/dengeldev" target="_blank"><IconTelegram/></Link>
+            <Link href="ttps://wa.me/+51977766744?text=Hola%20quiero%20más%20einformación" target="_blank"> <IconWhatsapp/></Link>      
+            <Link href="https://www.instagram.com/dengelrivera.dev/" target="_blank"><IconInstagram/> </Link>
+            <Link href="https://www.tiktok.com/@dengel_lionel?is_from_webapp=1&sender_device=pc" target="_blank"><IconTiktok/></Link>
+              
+              
             </ContenedorDatosFooter>
 
            <span className="text-white3">Diseño + Código © 2023</span>
