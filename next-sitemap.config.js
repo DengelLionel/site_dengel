ed318278233c3d30c9fd80fe8aaa95e4768a4e8b
DengelLionel <dengelriveracajincho@gmail.com>
2023-04-19 18:50:08 -0500

    sitemapp

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-/* const SITE_URL = process.env.SITE_URL || 'https://dengelriveradev.com';
+const SITE_URL = process.env.SITE_URL || 'https://dengelriveradev.com';
 
 
 const NEXT_SSG_FILES = [
@@ -21,72 +21,5 @@ const config = {
   },
 };
 
-export default config; */
-import { SitemapStream, streamToPromise } from 'sitemap'
-import { createGzip } from 'zlib'
-import { GetServerSidePropsContext } from 'next'
-import { createServer } from 'http'
+export default config; 
 
-const hostname = 'https://dengelriveradev.com'
-
-interface Url {
-  url: string
-  changefreq: string
-  priority: number
-}
-
-async function generateSitemapXml(): Promise<Buffer> {
-  const sitemap = new SitemapStream({
-    hostname,
-  })
-  const pipeline = sitemap.pipe(createGzip())
-
-  const urls: Url[] = [
-    {
-      url: '/',
-      changefreq: 'daily',
-      priority: 1,
-    },
-    {
-      url: '/about',
-      changefreq: 'monthly',
-      priority: 0.8,
-    },
-    // Agrega tus rutas aquí
-  ]
-
-  urls.forEach((url) => {
-    sitemap.write(url)
-  })
-
-  sitemap.end()
-
-  return streamToPromise(pipeline)
-}
-
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const sitemapXml = await generateSitemapXml()
-
-  ctx.res.setHeader('Content-Type', 'application/xml')
-  ctx.res.setHeader('Content-Encoding', 'gzip')
-  ctx.res.write(sitemapXml)
-  ctx.res.end()
-
-  return {
-    props: {},
-  }
-}
-
-const app = createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/xml')
-  res.setHeader('Content-Encoding', 'gzip')
-  generateSitemapXml().then((sitemapXml) => {
-    res.write(sitemapXml)
-    res.end()
-  })
-})
-
-app.listen(3000, () => {
-  console.log(`Server running on https://dengelriveradev.com`)
-})
