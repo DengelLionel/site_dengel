3be227a7a9d55bba335a5d643403f6b249bf3da7
DengelLionel <dengelriveracajincho@gmail.com>
2023-04-19 19:10:30 -0500

    sitem

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,26 +1,5 @@
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
+module.exports={
+siteUrl:"https://dengelriveradev.com",
+generateRobotsTxt:true,
 
-
-const NEXT_SSG_FILES = [
-    '/*.json$',
-    '/*_buildManifest.js$',
-    '/*_middlewareManifest.js$',
-    '/*_ssgManifest.js$',
-    '/*.js$',
-  ];
-  const config = {
-    siteUrl,
-    generateRobotsTxt: true,
-    exclude,
-    robotsTxtOptions: {
-      policies: [
-        {
-          userAgent: '*',
-          disallow: NEXT_SSG_FILES,
-        },
-      ],
-    },
-  };
-
-export default config; 
-
+}
